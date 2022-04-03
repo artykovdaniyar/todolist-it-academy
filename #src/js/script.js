a78d23171881fe1fef355a925659b8ffd2c76b2c
Daniir Artykov <artykovdaniyar@gmail.com>
2022-04-03 14:48:26 +0600

    rendering task & make it done

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,5 +1,15 @@
 import modal from "./modules/modal";
-
+import task from "./modules/task";
+let tasksList = [
+	{ title: "Заголовок задачи", description: "Более длинное описание задачи на несолько строчек, может быть даже на 4. Более длинное описание задачи на несолько строчек, может быть даже на 4", id: 111, done: true },
+	{ title: "Заголовок задачи", description: "Короткое описание", id: 222, done: false },
+	{ title: "Заголовок задачи без описания", description: "", id: 333, done: false },
+	{ title: "Заголовок задачи", description: "Короткое описание на 2 строчки", id: 444, done: false },
+	{ title: "Более длинный заголовок задачи на несолько строчек, может быть даже на 3. Более длинный заголовок", description: "", id: 555, done: true },
+	{ title: "Заголовок задачи", description: "Более длинное описание задачи на несолько строчек, может быть даже на 4. Более длинное описание задачи на несолько строчек, может быть даже на 4", id: 666, done: false },
+];
+task.updateTasks(tasksList, ".todo__list");
+task.addEventToInput(tasksList);
 const createTaskButton = document.querySelector(".header__btn");
 const modalCloseButtons = document.querySelectorAll(".modal__close-btn");
 const modalOverlay = document.querySelector(".overlay");
